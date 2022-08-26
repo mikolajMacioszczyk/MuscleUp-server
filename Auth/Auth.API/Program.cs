7eb1c487c8f6968876ea87b7147df2b6144c02bb
Mikołaj <macioszczyk.mikolaj@gmail.com>
2022-08-26 22:54:43 +0200

    use common in auth

--- Auth/Auth.API/Program.cs
+++ Auth/Auth.API/Program.cs
@@ -1,11 +1,10 @@
+using Common.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ProgramHelper.AddBasicApiServices(builder.Services);
 
 var app = builder.Build();
 
