8c289e0875cf8ba20f9fee975fbedf5ad4fda000
Mikołaj <macioszczyk.mikolaj@gmail.com>
2022-09-06 19:04:40 +0200

    specific permissions to own controllers

--- Carnets/Carnets.API/Controllers/PermissionController.cs
+++ Carnets/Carnets.API/Controllers/PermissionController.cs
@@ -10,26 +10,16 @@ namespace Carnets.API.Controllers
     [Route("[controller]")]
     public class PermissionController : ControllerBase
     {
-        private readonly IPermissionRepository<AllowedEntriesPermission> _allowedEntriesPermissionRepository;
-        private readonly IPermissionRepository<ClassPermission> _classPermissionRepository;
-        private readonly IPermissionRepository<TimePermissionEntry> _timePermissionRepository;
         private readonly IAssignedPermissionRepository _assignedPermissionRepository;
         private readonly IMapper _mapper;
 
-        public PermissionController(IMapper mapper,
-            IPermissionRepository<AllowedEntriesPermission> allowedEntriesPermissionRepository,
-            IPermissionRepository<ClassPermission> classPermissionRepository,
-            IPermissionRepository<TimePermissionEntry> timePermissionRepository, 
-            IAssignedPermissionRepository assignedPermissionRepository)
+        public PermissionController(IMapper mapper, IAssignedPermissionRepository assignedPermissionRepository)
         {
-            _allowedEntriesPermissionRepository = allowedEntriesPermissionRepository;
             _mapper = mapper;
-            _classPermissionRepository = classPermissionRepository;
-            _timePermissionRepository = timePermissionRepository;
             _assignedPermissionRepository = assignedPermissionRepository;
         }
 
-        [HttpGet("gympass/{gympassId}")]
+        [HttpGet("gympassPermissions/{gympassId}")]
         public async Task<ActionResult<IEnumerable<AssignedPermissionDto>>> GetAllGympassPermissions(string gympassId)
         {
             var permissionsResult = await _assignedPermissionRepository.GetAllGympassPermissions(gympassId);
@@ -97,129 +87,5 @@ namespace Carnets.API.Controllers
 
             return BadRequest(revokeResult.ErrorCombined);
         }
-
-        #region AllowedEntries
-
-        [HttpGet("allowedEntries")]
-        public async Task<ActionResult<IEnumerable<AllowedEntriesPermissionDto>>> GetAllAllowedEntriesPermisions() =>
-            await GetAllPermisions<AllowedEntriesPermission, AllowedEntriesPermissionDto>(_allowedEntriesPermissionRepository);
-
-        [HttpGet("allowedEntries/{permissionId}")]
-        public async Task<ActionResult<AllowedEntriesPermissionDto>> GetAllowedEntriesPermission([FromRoute] string permissionId) =>
-            await GetPermission<AllowedEntriesPermission, AllowedEntriesPermissionDto>(permissionId, _allowedEntriesPermissionRepository);
-
-        [HttpPost("allowedEntries")]
-        public async Task<ActionResult<AllowedEntriesPermissionDto>> CreateAllowedEntriesPermission([FromBody] CreateAllowedEntriesPermissionDto model)
-        {
-            var permission = _mapper.Map<AllowedEntriesPermission>(model);
-            return await CreatePermission<AllowedEntriesPermission, AllowedEntriesPermissionDto>(permission, _allowedEntriesPermissionRepository);
-        }
-
-        [HttpDelete("allowedEntries/{permissionId}")]
-        public async Task<ActionResult> DeleteAllowedEntriesPermission([FromRoute] string permissionId) =>
-            await DeletePermission(permissionId, _allowedEntriesPermissionRepository);
-
-        #endregion
-
-        #region ClassPermission
-
-        [HttpGet("class")]
-        public async Task<ActionResult<IEnumerable<ClassPermissionDto>>> GetAllClassPermisions() =>
-           await GetAllPermisions<ClassPermission, ClassPermissionDto>(_classPermissionRepository);
-
-        [HttpGet("class/{permissionId}")]
-        public async Task<ActionResult<ClassPermissionDto>> GetClassPermission([FromRoute] string permissionId) =>
-            await GetPermission<ClassPermission, ClassPermissionDto>(permissionId, _classPermissionRepository);
-
-        [HttpPost("class")]
-        public async Task<ActionResult<ClassPermissionDto>> CreateClassPermission([FromBody] CreateClassPermissionDto model)
-        {
-            var permission = _mapper.Map<ClassPermission>(model);
-            return await CreatePermission<ClassPermission, ClassPermissionDto>(permission, _classPermissionRepository);
-        }
-
-        [HttpDelete("class/{permissionId}")]
-        public async Task<ActionResult> DeleteClassPermission([FromRoute] string permissionId) =>
-           await DeletePermission(permissionId, _classPermissionRepository);
-
-        #endregion
-
-        #region TimePermissionEntry
-
-        [HttpGet("timeEntry")]
-        public async Task<ActionResult<IEnumerable<TimePermissionEntryDto>>> GetAllTimeEntryPermisions() =>
-           await GetAllPermisions<TimePermissionEntry, TimePermissionEntryDto>(_timePermissionRepository);
-
-        [HttpGet("timeEntry/{permissionId}")]
-        public async Task<ActionResult<TimePermissionEntryDto>> GetTimeEntryPermission([FromRoute] string permissionId) =>
-            await GetPermission<TimePermissionEntry, TimePermissionEntryDto>(permissionId, _timePermissionRepository);
-
-        [HttpPost("timeEntry")]
-        public async Task<ActionResult<TimePermissionEntryDto>> CreateTimeEntryPermission([FromBody] CreateTimePermissionEntryDto model)
-        {
-            var permission = _mapper.Map<TimePermissionEntry>(model);
-            return await CreatePermission<TimePermissionEntry, TimePermissionEntryDto>(permission, _timePermissionRepository);
-        }
-
-        [HttpDelete("timeEntry/{permissionId}")]
-        public async Task<ActionResult> DeleteTimeEntryPermission([FromRoute] string permissionId) =>
-           await DeletePermission(permissionId, _timePermissionRepository);
-
-        #endregion
-
-        #region Helpers
-
-        public async Task<ActionResult<IEnumerable<TPermissionDto>>> GetAllPermisions<TPermission, TPermissionDto>(
-            IPermissionRepository<TPermission> permissionRepository)
-            where TPermission : PermissionBase
-        {
-            return Ok(_mapper.Map<IEnumerable<TPermissionDto>>(await permissionRepository.GetAll()));
-        }
-
-        private async Task<ActionResult> GetPermission<TPermission, TPermissionDto>(string permissionId,
-            IPermissionRepository<TPermission> permissionRepository)
-            where TPermission : PermissionBase
-        {
-            var permission = await permissionRepository.GetPermissionById(permissionId);
-            if (permission != null)
-            {
-                return Ok(_mapper.Map<TPermissionDto>(permission));
-            }
-            return NotFound();
-        }
-
-        private async Task<ActionResult> CreatePermission<TPermission, TPermissionDto>(TPermission model,
-            IPermissionRepository<TPermission> permissionRepository)
-            where TPermission : PermissionBase
-        {
-            var createResult = await permissionRepository.CreatePermission(model);
-
-            if (createResult.IsSuccess)
-            {
-                return Ok(_mapper.Map<TPermissionDto>(createResult.Value));
-            }
-
-            return BadRequest(createResult.ErrorCombined);
-        }
-
-        public async Task<ActionResult> DeletePermission<TPermission>([FromRoute] string permissionId,
-            IPermissionRepository<TPermission> permissionRepository)
-            where TPermission : PermissionBase
-        {
-            var deleteResult = await permissionRepository.DeletePermission(permissionId);
-
-            if (deleteResult.IsSuccess)
-            {
-                return Ok();
-            }
-            else if (deleteResult.Errors?.Any(e => e.Equals(Common.CommonConsts.NOT_FOUND)) ?? false)
-            {
-                return NotFound();
-            }
-
-            return BadRequest(deleteResult.ErrorCombined);
-        }
-
-        #endregion
     }
 }
