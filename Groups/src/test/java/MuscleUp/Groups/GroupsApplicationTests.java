13bf853721fac910851de805c16f14d80ad38fa2
mateuszkleinowski <0kleinowski0@gmail.com>
2022-09-08 19:18:05 +0200

    Init separate services with web dependencies

---
+++ Groups/src/test/java/MuscleUp/Groups/GroupsApplicationTests.java
@@ -0,0 +1,13 @@
+package MuscleUp.Groups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
